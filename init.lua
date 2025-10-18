ceadcbfb3f26982e75a8bd518c39746c4f8de2a7
nochzato <nochzato@gmail.com>
2025-10-18 16:47:29 +0300

    add mini.move and disable word wrap

--- init.lua
+++ init.lua
@@ -116,6 +116,9 @@ vim.o.mouse = 'a'
 -- Don't show the mode, since it's already in the status line
 vim.o.showmode = false
 
+-- Disable word wrap
+vim.o.wrap = false
+
 -- Sync clipboard between OS and Neovim.
 --  Schedule the setting after `UiEnter` because it can increase startup-time.
 --  Remove this option if you want your OS clipboard to remain independent.
@@ -938,6 +941,8 @@ require('lazy').setup({
       -- - sd'   - [S]urround [D]elete [']quotes
       -- - sr)'  - [S]urround [R]eplace [)] [']
       require('mini.surround').setup()
+
+      require('mini.move').setup()
 
       -- Simple and easy statusline.
       --  You could remove this setup call if you don't like it,
